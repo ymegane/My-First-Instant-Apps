8494d9ee2ac324ba002e5f5887c078c077a9100b
y_megane <airhole.ymegane+github@gmail.com>
2017-05-18 13:24:46 +0900

    :+1: Reference from base module

---
+++ base/src/main/java/com/github/ymegane/android/myfirstinstantapp/base/Utils.java
@@ -0,0 +1,9 @@
+package com.github.ymegane.android.myfirstinstantapp.base;
+
+import android.util.Log;
+
+public class Utils {
+    public static void something() {
+        Log.v("tag", "aaa");
+    }
+}
--- feature/src/main/java/com/github/ymegane/android/myfirstinstantapp/feature/MainActivity.java
+++ feature/src/main/java/com/github/ymegane/android/myfirstinstantapp/feature/MainActivity.java
@@ -3,11 +3,15 @@ package com.github.ymegane.android.myfirstinstantapp.feature;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.ymegane.android.myfirstinstantapp.base.Utils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Utils.something();
     }
 }
