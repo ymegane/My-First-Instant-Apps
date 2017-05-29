7fc42ef76a2b2e9276256f5dd505bf1ea493da24
y_megane <airhole.ymegane+github@gmail.com>
2017-05-29 10:28:50 +0900

    :+1: Add launcher activity to base module

---
+++ base/src/main/java/com/github/ymegane/android/myfirstinstantapp/base/DummyActivity.java
@@ -0,0 +1,13 @@
+package com.github.ymegane.android.myfirstinstantapp.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class DummyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
